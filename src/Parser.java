168015f1e757bd491090a0a60c891762f5ac876d
safamSaol <yosi_dekel@walla.com>
2019-11-23 16:36:16 +0200

    readFile + parser and main

--- src/Parser.java
+++ src/Parser.java
@@ -1,18 +1,38 @@
 import java.util.*;
-
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 public class Parser {
 
     private Map<String, ArrayList<String>> termMap;
     private HashSet<String> stopwords;
 
-    public Parser(){
+    public Parser(ReadFile read) throws IOException {
         termMap = new HashMap<String, ArrayList<String>>();
         stopwords = new HashSet<String>();
         //add stopwords to hashset
+        /*
+        this part of the code is from https://howtodoinjava.com/java/io/read-file-from-resources-folder/
+         */
+        String fileName = "stopwords.txt";
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        File stopWordsFile = new File(classLoader.getResource(fileName).getFile());
+        String stopContent = new String(Files.readAllBytes(stopWordsFile.toPath()));
+        String stopLines[] = stopContent.split("\\r?\\n");
+        stopwords.addAll(Arrays.asList(stopLines));
+        System.out.println(stopwords.size());
+        ///////////////
+        parseDocs(read.allFile);
     }
 
-    public void parseDocs (ArrayList<String> docList){
-
+    public void parseDocs (ArrayList<String> docList) {
+        for (int i=0;i<docList.size();i++) {
+            if(!docList.get(i).equals("\n")&&!docList.get(i).equals("\n\n\n")&&!docList.get(i).equals("\n\n\n\n")&&!docList.get(i).equals("\n\n")){
+                String docId = docList.get(i);
+                String result = docId.substring(docId.indexOf("<DOCNO>")+8 , docId.indexOf("</DOCNO>")-1);
+                String txt =  docId.substring(docId.indexOf("<TEXT>")+7 , docId.indexOf("</TEXT>"));
+                String[] tokens = txt.split("\\s+|\n");
+            }
+        }
     }
-
 }
--- src/ReadFile.java
+++ src/ReadFile.java
@@ -8,36 +8,34 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 public class ReadFile {
-    public static void main(String[] args) throws IOException {
-        System.out.println("Enter the path to search");
-        String doc = "";
-        int counter=0;
-        List<String> allFile = new ArrayList<>();
+    protected ArrayList<String>allFile;
+
+    public ReadFile() throws IOException {
+        allFile = new ArrayList<>();
+        int counter = 0;//delete
         Scanner scanner = new Scanner(System.in);
         String folderPath = scanner.next();
-        File [] files1 = null;
+        File[] files1 = null;
         File folder = new File(folderPath);
         if (folder.isDirectory()) {
             File[] listOfSubFolders = folder.listFiles();
             System.out.println(listOfSubFolders.length);
-
             for (File SubFolder : listOfSubFolders) {
-                if(SubFolder.isDirectory()) {
+                if (SubFolder.isDirectory()) {
                     files1 = SubFolder.listFiles();
-                    for(File file2 : files1) {
-
+                    for (File file2 : files1) {
                         String TxtPaths = file2.getPath();
                         Scanner file3 = new Scanner(file2);
                         String text = new String(Files.readAllBytes(Paths.get(TxtPaths)), StandardCharsets.UTF_8);
                         String[] splits = text.split("</DOC>");
                         allFile.addAll(Arrays.asList(splits));//
-                        counter = counter + splits.length-1;
+                        counter = counter + splits.length - 1;//delete
 
                     }
                 }
             }
         }
-        System.out.println(counter);
-        System.out.println(allFile.size()-1815);
+        System.out.println(counter);//delete
+        System.out.println(allFile.size() - 1815);//delete
     }
 }
