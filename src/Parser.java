be75d7b562f85dbf092d8b8e57e6384710620491
yinonbz <yinonbz@gmail.com>
2019-11-23 12:14:19 +0200

    parser and stopwords

---
+++ src/Parser.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Parser {
+
+    private Map<String, ArrayList<String>> termMap;
+    private HashSet<String> stopwords;
+
+    public Parser(){
+        termMap = new HashMap<String, ArrayList<String>>();
+        stopwords = new HashSet<String>();
+        //add stopwords to hashset
+    }
+
+    public void parseDocs (ArrayList<String> docList){
+
+    }
+
+}
